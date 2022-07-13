b7f4604620aac7d59fa75af70f3f683d0fae7819
Tuomas Kontola <tuomas.kontola@columbiaroad.com>
2022-07-13 13:21:46 +0300

    add currentQuantity to itemCategories

--- src/api/item-category/controllers/item-category.ts
+++ src/api/item-category/controllers/item-category.ts
@@ -4,4 +4,48 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::item-category.item-category');
+type Data = {
+  id: number;
+  attributes: {
+    [key: string]: string,
+  }
+}
+export default factories.createCoreController('api::item-category.item-category', {
+  async findOne(ctx) {
+    const { data, meta } = await super.findOne(ctx);
+    const totalCategoryItemCount = await strapi.query('api::item.item').count({
+      where: {
+        itemType: {
+          itemCategory: {
+            id: data.id
+          }
+        }
+      },
+    });
+    data.attributes.currentQuantity = totalCategoryItemCount;
+    return { data, meta };
+  },
+  async find(ctx) {
+    const { data, meta } = await super.find(ctx);
+    const mappedPromises = (data as Data[]).map(async (newData: Data) => {
+      const totalCategoryItemCount = await strapi.query('api::item.item').count({
+        where: {
+          itemType: {
+            itemCategory: {
+              id: newData.id
+            }
+          }
+        },
+      });
+      return {
+        id: newData.id,
+        attributes: {
+          ...newData.attributes,
+          currentQuantity: totalCategoryItemCount,
+        }
+      };
+    });
+    const mappedData = await Promise.all(mappedPromises);
+    return { data: mappedData, meta };
+  }
+});
