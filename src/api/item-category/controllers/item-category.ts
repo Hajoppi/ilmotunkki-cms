8abe3078b09d21835a0ddfafea0289edc7a6e6f4
Tuomas Kontola <tuomas.kontola@columbiaroad.com>
2022-08-21 11:13:55 +0300

    Adds item availability filtering

--- src/api/item-category/controllers/item-category.ts
+++ src/api/item-category/controllers/item-category.ts
@@ -4,12 +4,33 @@
 
 import { factories } from '@strapi/strapi'
 
-type Data = {
+export type ItemType = StrapiBaseType<{
+  price: number;
+  availableFrom: string;
+  availableTo: string;
+  itemCategory: StrapiResponse<ItemCategory>;
+  slug: string;
+}>;
+export type ItemCategory = StrapiBaseType<{
+  orderItemLimit: number;
+  maximumItemLimit: number;
+  name: string;
+  itemTypes: StrapiResponse<ItemType[]>;
+  currentQuantity: number;
+  overflowItem: StrapiResponse<ItemType | null>;
+}>;
+export type StrapiBaseType<T> = {
   id: number;
-  attributes: {
-    [key: string]: string,
-  }
+  attributes: T & {
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+  };
+};
+type StrapiResponse<T> = {
+  data: T
 }
+
 export default factories.createCoreController('api::item-category.item-category', {
   async findOne(ctx) {
     const { data, meta } = await super.findOne(ctx);
@@ -27,7 +48,20 @@ export default factories.createCoreController('api::item-category.item-category'
   },
   async find(ctx) {
     const { data, meta } = await super.find(ctx);
-    const mappedPromises = (data as Data[]).map(async (newData: Data) => {
+    const categories = data as ItemCategory[];
+    const now = new Date();
+    const filteredData = categories.reduce((acc,category) => {
+      const filteredItemTypes = category.attributes.itemTypes.data.filter(itemType => {
+        if(!itemType.attributes.availableFrom || !itemType.attributes.availableTo) return true;
+        const availableFrom = new Date(itemType.attributes.availableFrom);
+        const availableTo = new Date(itemType.attributes.availableTo);
+        return now > availableFrom && now < availableTo;
+      });
+      category.attributes.itemTypes.data = filteredItemTypes;
+      acc.push(category);
+      return acc;
+    },[] as ItemCategory[]);
+    const mappedPromises = filteredData.map(async (newData: ItemCategory) => {
       const totalCategoryItemCount = await strapi.query('api::item.item').count({
         where: {
           itemType: {
