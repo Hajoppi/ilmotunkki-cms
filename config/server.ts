06894d14d419dc9fa77e78419378dad5cba0c63c
Tuomas Kontola <tuomas.kontola@columbiaroad.com>
2022-08-23 21:17:04 +0300

    Adds url

--- config/server.ts
+++ config/server.ts
@@ -2,6 +2,7 @@ import cronTasks from './cron-tasks';
 
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
+  url: env('URL', ''),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
