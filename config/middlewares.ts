1de1bd2f607b687d527fda30a101910cfe12dead
Tuomas Kontola <tuomas.kontola@columbiaroad.com>
2023-01-05 18:05:48 +0200

    Remove defaults

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -4,7 +4,6 @@ export default [
     name: 'strapi::security',
     config: {
       contentSecurityPolicy: {
-        useDefaults: true,
         directives: {
           "connect-src": ["'self'", "https:"],
           "img-src": [
